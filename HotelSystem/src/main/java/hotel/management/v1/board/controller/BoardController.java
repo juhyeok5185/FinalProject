c53c326db54eea980ca832f94a25d2ef9de22302
atoonatoo <129242580+atoonatoo@users.noreply.github.com>
2023-04-13 13:54:09 +0900

    동욱
    1

---
+++ HotelSystem/src/main/java/hotel/management/v1/board/controller/BoardController.java
@@ -0,0 +1,37 @@
+package hotel.management.v1.board.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import hotel.management.v1.board.dao.BoardDao;
+import hotel.management.v1.board.dto.BoardDto;
+import hotel.management.v1.board.entity.Board;
+
+@Controller
+public class BoardController {
+	@Autowired
+	private BoardDao boarddao;
+	
+	@GetMapping("/hotel/board/write")
+	public void write() {
+		
+	}
+	
+	@PostMapping("/hotel/board/write")
+	public void write(BoardDto.BoardDtoWrite dto) {
+		System.out.println(dto);
+		boarddao.write(dto);
+	}
+	
+	@GetMapping("/hotel/board/list")
+	public ModelAndView findAll(BoardDto.FindAll dto) {
+		List<Board> list = boarddao.findAll(dto);
+		return new ModelAndView("board").addObject("board", list);
+	}
+}
