25f4a28fa5e833977c943e3350ae2fb7a02aa08e
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-04-13 10:56:06 +0900

    added

--- HotelSystem/src/main/java/hotel/management/v1/manager/controller/managerController.java
+++ HotelSystem/src/main/java/hotel/management/v1/manager/controller/managerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/hotel")
 public class managerController {
     
-    @GetMapping("/manager/booklist")
+    @GetMapping("/manager/bookList")
     public void booklist(){
         
     }
