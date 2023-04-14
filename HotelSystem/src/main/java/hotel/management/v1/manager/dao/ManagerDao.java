c20946b40a31ed94d08ad58d45b6cc32f8f87b42
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-04-14 11:19:04 +0900

    added

--- HotelSystem/src/main/java/hotel/management/v1/manager/dao/ManagerDao.java
+++ HotelSystem/src/main/java/hotel/management/v1/manager/dao/ManagerDao.java
@@ -1,8 +1,15 @@
 package hotel.management.v1.manager.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import hotel.management.v1.manager.dto.ManagerDto;
 
 @Mapper
 public interface ManagerDao {
+
+    public List<ManagerDto.test> test();
     
 }
