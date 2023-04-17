e4b31d140b8a680719606ab23fefb83cd61303fc
cpsua97 <cpsua97@naver.com>
2023-04-17 09:12:01 +0900

    wt
    wt

--- HotelSystem/src/main/java/hotel/management/v1/member/controller/MemberController.java
+++ HotelSystem/src/main/java/hotel/management/v1/member/controller/MemberController.java
@@ -3,12 +3,15 @@ package hotel.management.v1.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ch.qos.logback.core.joran.conditional.IfAction;
 import hotel.management.v1.member.dto.MemberDto;
 import hotel.management.v1.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/hotel")
@@ -29,8 +32,15 @@ public class MemberController {
 		return "redirect:/hotel/member/joincomplete";
     }
     
+    @PreAuthorize("isAnonymous()")
     @GetMapping("/member/login")
-    public void login() {}
+    public void login(HttpSession session, Model model) {
+    	if (session.getAttribute("msg")!=null) {
+			model.addAttribute("msg", session.getAttribute("msg"));
+			session.removeAttribute("msg");
+		}
+    }
+    	
     
     @GetMapping("/member/find")
     public void find() {
