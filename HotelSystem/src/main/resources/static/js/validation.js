3a696ec3a71cd19bf8482b822054b6a5a3b6061c
cpsua97 <cpsua97@naver.com>
2023-04-17 13:23:52 +0900

    ㅈㅅ
    ㅈㅅ

--- HotelSystem/src/main/resources/static/js/validation.js
+++ HotelSystem/src/main/resources/static/js/validation.js
@@ -34,35 +34,34 @@ function telCheck() {
 	return check($('#tel').val(),pattern,"휴대전화 형식에 맞게 입력하세요",$('#tel_msg'));
 }
 
-
-
-
-
-
-
-
-
 function usernameCheck() {
 	const value = $("#username").val();
 	$('#username').val(value);
 	return check(value,/^[a-zA-Z0-9]{7,10}$/, "아이디는 문자, 숫자의 조합 7~10자입니다",$('#username_msg'));
 }
 
+function passwordCheck() {
+	$('#password_msg').text("");
+	const pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+	return check($('#password').val(), pattern, "비밀번호는 문자, 숫자, 특수문자의 조합 최소 8자리입니다.", $("#password_msg"));
+}
 
-
-
-
-
-
-
-
-
-
+function password2Check() {
+	$('#password2_msg').text("");
+	const value =$('#password2').val();
+	if(value=="")	{
+		$('#password2_msg').text("필수입력입니다").attr("class","fail");
+		return false;
+	}
+	if(value!=$('#password').val()) {
+		$('#password2_msg').text("비밀번호가 일치하지 않습니다.").attr("class","fail");
+		return false;
+	}
+}
 
 $(document).ready(function() {
 	$('#name').blur(nameCheck);
 	$('#personalId').blur(personalIdCheck);
-	
 	$('#email').blur(async function() {
 		if(emailCheck()==false) {
 			return false;
@@ -75,27 +74,32 @@ $(document).ready(function() {
 		}
 	});
 	$('#tel').blur(telCheck);
-	
-	
-	
-	
-	
 	$('#username').blur(async function() {
 		if(usernameCheck()==false) {
 			return false;
 		}
 		try {
 			await $.ajax('/hotel/member/check/username?username='+$('#username').val());
-			$("#username_msg").text("좋은아이디네요.").attr("class","success");
+			$("#username_msg").text("사용가능한아이디입니다.").attr("class","success");
 		} catch(err) {
 			$("#username_msg").text("이미사용중입니다.").attr("class","fail");
 		}
-	})
-	
-	
-	
-	
+	});
+	$('#password').blur(passwordCheck);
+	$('#password2').blur(password2Check);
 	$('#join').click(function() {
-		$('#join_form').submit();
+		const result = nameCheck() && personalIdCheck() && emailCheck() && telCheck() && usernameCheck() && passwordCheck() && password2Check();
+		if(result==false) {
+			return false;
+		}
+		try {
+			await $.ajax('/hotel/member/check/email?email=' + $('#email').val());
+			await $.ajax('/hotel/member/check/username?username=' + $('#username').val());
+			$('#join_form').submit();
+		} catch(err) {
+			console.log(err);
+			alert("아이디나 이메일이 사용 중입니다");
+		}
+		
 	})
 })
