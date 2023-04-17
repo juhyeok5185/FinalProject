0b36b0c56668901028f61ba2fff9576dc380f456
AromaJak <slalszkf1@naver.com>
2023-04-18 08:45:16 +0900

    css+백작업

--- HotelSystem/src/main/java/hotel/management/v1/client/cotroller/bookController.java
+++ HotelSystem/src/main/java/hotel/management/v1/client/cotroller/bookController.java
@@ -1,13 +1,22 @@
 package hotel.management.v1.client.cotroller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/hotel")
 public class bookController {
 	@GetMapping("/client/roombook")
-	public void book() {
+	public void bookpage() {
+	}
+	
+	@PostMapping("/client/roombook")
+	public void roombook(Principal pal) {
+		String username =  pal.getName();
+		
 	}
 }
