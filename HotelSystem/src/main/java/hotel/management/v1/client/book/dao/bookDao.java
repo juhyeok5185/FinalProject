5b16c445b08ee02f2ff433e34b57c57ede9c1df1
AromaJak <slalszkf1@naver.com>
2023-04-19 15:35:42 +0900

    방예약백단

--- HotelSystem/src/main/java/hotel/management/v1/client/book/dao/bookDao.java
+++ HotelSystem/src/main/java/hotel/management/v1/client/book/dao/bookDao.java
@@ -5,10 +5,21 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import hotel.management.v1.client.book.dto.bookDto;
+import hotel.management.v1.client.book.dto.bookDto.addRoomBook;
+import hotel.management.v1.client.book.dto.bookDto.addbook;
+import hotel.management.v1.client.book.dto.bookDto.book;
 
 @Mapper
 public interface bookDao {
 	
 	public List<bookDto.findRoom> findRoomFromAndTo(String from, String to);
+
+	public Integer addBook(addbook adbo);
+
+	public Integer addRoomBooking(addRoomBook adrb);
+	
+
+
+
 
 }
