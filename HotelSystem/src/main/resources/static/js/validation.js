880d7dccb023fa5ceb8b017f82ff32a90ebfc7fa
cpsua97 <cpsua97@naver.com>
2023-04-20 08:39:25 +0900

    wtwt
    ㅈㅅ

--- HotelSystem/src/main/resources/static/js/validation.js
+++ HotelSystem/src/main/resources/static/js/validation.js
@@ -112,7 +112,6 @@ $(document).ready(function() {
 	
 		$('#changepasswordbtn').click(function() {
 			const changeresult = passwordCheck() && password2Check();
-			console.log(changeresult);
 			if(changeresult==false) {
 				alert('비밀번호를 확인해주세요.');
 				return false;
