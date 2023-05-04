5719efe7eb65dc7f209b52abb23386df2942dc55
atoonatoo <129242580+atoonatoo@users.noreply.github.com>
2023-05-04 15:57:01 +0900

    동욱
    1

--- HotelSystem/src/main/java/hotel/management/v1/board/controller/BoardController.java
+++ HotelSystem/src/main/java/hotel/management/v1/board/controller/BoardController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import hotel.management.v1.board.dto.BoardDto;
-import hotel.management.v1.board.entity.Board;
 import hotel.management.v1.board.service.BoardService;
 
 // 관리자 = 김동욱
@@ -19,49 +18,43 @@ import hotel.management.v1.board.service.BoardService;
 @Controller
 public class BoardController {
 	@Autowired
-	private BoardService boardService;
+	private BoardService service;
 
 	@GetMapping("/hotel/board/write")
 	public ModelAndView write(Principal principal) {
-		return new ModelAndView("/hotel/board/write").addObject("name",principal.getName());
+		return new ModelAndView("/hotel/board/write").addObject("name", principal.getName());
 	}
 
 	@PostMapping("/hotel/board/write")
-	public ModelAndView write(BoardDto.Write dto ,Principal principal) {
-		boardService.write(dto, principal.getName());
-		 return new ModelAndView("redirect:/hotel/board/list");
+	public ModelAndView write(BoardDto.Write dto, Principal principal) {
+		service.write(dto, principal.getName());
+		return new ModelAndView("redirect:/hotel/board/list");
 	}
 
 	@GetMapping("/hotel/board/list")
 	public ModelAndView list(@RequestParam(defaultValue = "1") Integer pageno) {
-		BoardDto.Pagination list = boardService.pagination(pageno);
-		return new ModelAndView("/hotel/board/list")
-				.addObject("board", list.getBoard())
-				.addObject("prev",	list.getPrev())
-				.addObject("start",  list.getStart())
-				.addObject("end",	list.getEnd())
-				.addObject("next",	list.getNext())
-				.addObject("pageno", pageno);
+		BoardDto.Pagination list = service.pagination(pageno);
+		return new ModelAndView("/hotel/board/list").addObject("board", list.getBoard())
+				.addObject("prev", list.getPrev()).addObject("start", list.getStart()).addObject("end", list.getEnd())
+				.addObject("next", list.getNext()).addObject("pageno", pageno);
 	}
 
 	@GetMapping("/hotel/board/read")
 	public void read(Integer boardNo, Model model) {
-		Board board = boardService.findByNo(boardNo);
-		model.addAttribute("board", boardService.findByNo(boardNo));
+		service.findByNo(boardNo);
+		model.addAttribute("board", service.findByNo(boardNo));
 	}
 
 	@PostMapping("/hotel/board/read")
 	public ModelAndView reply(Integer boardNo, String replyContent, String username) {
-		boardService.replyUpdate(boardNo, replyContent, username);
+		service.replyUpdate(boardNo, replyContent, username);
 		return new ModelAndView("redirect:/hotel/board/read?boardNo=" + boardNo);
 	}
-	
 
 	@PostMapping("/hotel/board/delete")
-	public ModelAndView deleteBoard(String boardNo) {
-	    boardService.delete(boardNo);
-	        return new ModelAndView("redirect:/hotel/board/list");
+	public ModelAndView delete(String boardNo, String username, Principal principal) {
+		String pusername = principal.getName();
+		service.delete(boardNo);
+		return new ModelAndView("redirect:/hotel/board/list").addObject("msg","삭제가 완료되었습니다.");
 	}
-	
-	
 }
