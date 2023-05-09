66b0115da80f86dc9e3318d92995369d516d357f
dlrkdms23 <129341268+dlrkdms23@users.noreply.github.com>
2023-05-09 16:49:48 +0900

    dlrkdsm
    dlrkdms

--- HotelSystem/src/main/resources/static/js/mall.js
+++ HotelSystem/src/main/resources/static/js/mall.js
@@ -15,18 +15,19 @@ class item {
 };
 
 function order(pickupDay, tbodyArray) {
-		$.ajax({
-			type: 'post',
-			url: '/hotel/mall/order',
-			data: {
-				pickupDay : pickupDay,
-				tbodyArray : JSON.stringify(tbodyArray)
-			},
-			success: function(result) {
-			}, error: function(error){
-				console.log("실패");
-			}
-		})
+	$.ajax({
+		type: 'post',
+		url: '/hotel/mall/order',
+		data: {
+			pickupDay : pickupDay,
+			tbodyArray : JSON.stringify(tbodyArray)
+		},
+		success: function(result) {
+		}, 
+		error: function(error) {
+			console.log("실패");
+		}
+	})
 };
 
 // 카카오JS
@@ -49,132 +50,32 @@ function kakaojs(result) {
 	})
 };
 
+const mallItems=[
+	{index: 1 , count:0 , price:3360000, name : 'BEDDING SET', stock:3},
+	{index: 2 , count:0 , price:20000, name : 88, stock:3},
+	{index: 3 , count:0 , price:20000, name : 88, stock:3},
+	{index: 4 , count:0 , price:20000, name : 88, stock:3},
+	{index: 5 , count:0 , price:20000, name : 88, stock:3},
+	{index: 6 , count:0 , price:20000, name : 88, stock:3},
+];
+
+
 $(document).ready(function() {
 	let checkBtn = false;
-	let count = 1;
 	let click = 0;
-	const stock = 3;
 	let trCount = 0;
 	var clientKey = 'test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq';
 	var tossPayments = TossPayments(clientKey);
-	
-	// 상품담기	
-	$('.item-btn').click(function() {
-		// 요금합계
-		let index = $(this).data('index');
-		let name = $(`#luxurytem-${index} .itemName`).text();
-		let price = parseInt($(`#luxurytem-${index} .itemPrice`).text().replace(/,/g, ""));
-		
-		
-		// 선택버튼 클릭횟수 제한
-		let clickCnt = parseInt($(this).attr('data-click-cnt')) || 0;
-  		if(clickCnt >= stock) {
-    		alert('최대 '+stock+'개까지만 선택할 수 있습니다.');
-    		return;
-		} 
-  		clickCnt++;
-  		$(this).attr('data-click-cnt', clickCnt);
-		
-		
-		// 요금상세
-		const $tbody = $("#tbody");
-		let existingItem = false;
-		let $existingRow = null;
-		$("#tbody tr").each(function() {
-			if ($(this).find("#tbodyName").text() == name) {
-				existingItem = true;
-				$existingRow = $(this);
-				return false;
-			}
-		});
+	let cartClick = 0;
 
-		if (existingItem) {
-			let $count = $existingRow.find('#count');
-			let count = parseInt($count.text());
-			count++;
-			$count.text(count);
-
-			let $price = $existingRow.find('#price');
-			let existingPrice = parseInt($price.text());
-			existingPrice += price;
-			$price.text(existingPrice);
-		} else {
-			const tpl = `
-	            <tr>
-	                <td class="tbodyName" id="tbodyName" style="line-height:50px">${name}</td>
-	                <td style="line-height:50px">
-	                    <button class="btn btn-outline-secondary  minus" data-index=${index}>-<input type="hidden" value="${price}"></button>
-	                    <span id="count">${count}</span>
-	                    <button class="btn btn-outline-secondary plus" data-index=${index}>+<input type="hidden" value="${price}"></button>
-	                </td>
-	                <td id="price" style="line-height:50px">${price}</td>
-	                <td style="line-height:50px"><button class="btn btn-outline-secondary delete" data-index=${index}>X</button></td>
-	            </tr>`;
-			$tbody.append(tpl);
-			trCount++;
-		}
-		const totalPrice = $('#totalPrice');
-		tPrice += price;
-		totalPrice.text(tPrice.toLocaleString() + ' 원');
-	});
 	
-	// 상품개수 감소
-	$(document).on('click', '.minus', function() {
-		const $count = $(this).parent().find('span');
- 		let count = parseInt($count.text());
-		if(count <= 1) {
-			alert('상품은 1개 이상부터 구매가 가능합니다.');
-			return;
-		}
-		$count.text(--count);		
-		
-		const $price = $(this).parent().next();
-		let index = $(this).data('index');
-		let itemPrice = parseInt($(`#luxurytem-${index} .itemPrice`).text().replace(/,/g, ""));
-		let price = parseInt($price.text());	
-		price = price-itemPrice;
-		$price.text(price);
-		let $input = parseInt($(this).find('input[type="hidden"]').val());
-		totalPrice(-$input);
-	});
-	
-	// 상품개수 증가
-	$('#tbody').on('click', '.plus', function() {
-		const $count = $(this).parent().find('span');
-		let count = parseInt($count.text());
-		if(count >= stock) {
-			alert(stock+'개 이상 구매가 불가합니다.');
-			return;
-		}
-		$count.text(++count);
-		
-		const $price = $(this).parent().next();
-		let index = $(this).data('index');
-		let itemPrice = parseInt($(`#luxurytem-${index} .itemPrice`).text().replace(/,/g, ""));
-		let price = parseInt($price.text());
-		price = price+itemPrice;
-		$price.text(price);
-		
-		var $input = parseInt($(this).find('input[type="hidden"]').val());
-		totalPrice($input);
-	});
-	
-	// 상품삭제
-	$('#tbody').on('click', '.delete', function() {
-		const $tr = $(this).parent().parent();
-		
-		const $input = parseInt($(this).parent().prev().text());
-		totalPrice(-$input);
-		$tr.remove();
-		trCount--;
-	});
 	
 	// 결제 상세정보
 	$('#chargeInquiry').on('click', function() {
-		var content = document.getElementById("totalBillDetailInner");
+		console.log('aaaaaaaaaa');
+		let content = document.getElementById("totalBillDetailInner");
 		++click;
 		if (checkBtn == true) {
-			console.log("true");
 		} else if (checkBtn == false) {
 			if (click % 2 != 0) {
 				$('html').attr('style', 'overflow: hidden');
@@ -217,7 +118,7 @@ $(document).ready(function() {
 	$('#choosePayment-box1').click(function() {
 		let pickupDay = $('.pickup').val();
 		let itemPrice = parseInt($(`#totalPrice`).text().replace(/,/g, ""));
-		let itemName = `${trCount-1}`==0?$(`#tbodyName`).text().replace(/ /g, ""):$(`#tbodyName`).text().replace(/ /g, "")+` 외${trCount-1}건`;
+		let itemName = `${trCount-1}`==0?$(`.tbodyName`).text().replace(/ /g, ""):$(`.tbodyName`).text().replace(/ /g, "")+` 외${trCount-1}건`;
 		const tbodyArray = []; 
 		
 		$('td.tbodyName').each(function() { 
@@ -229,7 +130,7 @@ $(document).ready(function() {
 	$('#choosePayment-box2').click(function() {
 		let pickupDay = $('.pickup').val();
 		let itemPrice = parseInt($(`#totalPrice`).text().replace(/,/g, ""));
-		let itemName = `${trCount-1}`==0?$(`#tbodyName`).text().replace(/ /g, ""):$(`#tbodyName`).text().replace(/ /g, "")+` 외${trCount-1}건`;
+		let itemName = `${trCount-1}`==0?$(`.tbodyName`).text().replace(/ /g, ""):$(`.tbodyName`).text().replace(/ /g, "")+` 외${trCount-1}건`;
 		let uuid = self.crypto.randomUUID();
 		const tbodyArray = []; 
 		
@@ -238,7 +139,6 @@ $(document).ready(function() {
 		});
 		
 		order({pickupDay,tbodyArray});
-		console.log(tbodyArray);
 		tossPayments.requestPayment('TOSSPAY', {
 			amount: itemPrice,
 			orderId: uuid,
@@ -248,19 +148,17 @@ $(document).ready(function() {
 		})
 	})
 	
-		// 반응형
+	/* 반응형 */
+	// 카트팝업 
 	$('.mallCart-icon').click(function() {
-		console.log('1111111111');
+		console.log();
 		if (cartClick%2!=0) {
-		  console.log('22222222222');
-		  $('.mallCart-popup').attr('style', 'display:block');
-		} else {
-		  console.log('3333333333333');
 		  $('.mallCart-popup').attr('style', 'display:none');
+		  $('.mallCart-content').attr('style', 'display:none');
+		} else {
+		  $('.mallCart-popup').attr('style', 'display:block');
+		  $('.mallCart-content').attr('style', 'display:block');
 		}
 		++cartClick;
 	  });
-	
-
-	
 });
