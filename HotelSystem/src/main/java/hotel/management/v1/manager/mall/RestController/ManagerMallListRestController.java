14ccc2aa4f70628dd8c04088a10936354c551ed4
atoonatoo <129242580+atoonatoo@users.noreply.github.com>
2023-05-15 19:28:24 +0900

    동욱
    1

--- HotelSystem/src/main/java/hotel/management/v1/manager/mall/RestController/ManagerMallListRestController.java
+++ HotelSystem/src/main/java/hotel/management/v1/manager/mall/RestController/ManagerMallListRestController.java
@@ -21,6 +21,7 @@ public class ManagerMallListRestController {
 	@PostMapping("/hotel/manager/managerMallList")
 	public ResponseEntity<List<ManagerMallListDto.MallListSearch>> mallList(ManagerMallListDto.FindMallList dto) {
 		List<ManagerMallListDto.MallListSearch> list = service.mallsearch(dto);
+		System.out.println(service.mallsearch(dto));
 		return ResponseEntity.ok(list);
 	}
 
