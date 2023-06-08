faa0698d3be0d3a3e208749079973ca072a10e15
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-06-08 10:56:14 +0900

    added

--- 김동현/managementjs.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.list-group-item').click(function(){
-    const $listitem = $('.list-group-item');
-    $($listitem).attr("class",'list-group-item');
-    const target = this;
-    $(target).attr("class","list-group-item active");
-  })
-
-})
